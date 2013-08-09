e7ddf06dfe50b7fe38a316d9df9e12d63e63392e
Fatteru <gaodaojing@gmail.com>
2013-08-09 17:44:17 +0800

    recover merge error

--- init.el
+++ init.el
@@ -19,53 +19,41 @@
 
 ;; }}}
 
-;; Initialise melpa {{{
+;; Initialise el-get {{{
 
   (require 'package)
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/") t)
   (package-initialize)
 
-  ;; Work around MELPA server issues
-  (setq url-http-attempt-keepalives nil)
+  (setq el-get-user-package-directory "~/.emacs.d/el-get-init-files/")
+  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-  ;; Refreshing the MELPA repo
-  (when (not package-archive-contents)
-    (package-refresh-contents))
+  (unless (require 'el-get nil 'noerror)
+    (with-current-buffer
+      (url-retrieve-synchronously
+        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+      (let (el-get-master-branch)
+        (goto-char (point-max))
+        (eval-print-last-sexp))))
 
-  ;; Making sure melpa.el is around for the other installations
-  (unless (require 'melpa nil t)
-    (package-install 'melpa)
-    (require 'melpa))
+  ;; personal recipes
+  (setq el-get-sources
+        '((:name el-get :branch "master")))
 
-  ;; Making sure my packages are installed
-  (defvar my-packages '(autopair
-                        yasnippet
-                        auto-complete
-                        markdown-mode
-                        color-theme-solarized
-                        powerline
-                        slim-mode
-                        css-mode
-                        sass-mode
-                        scss-mode
-                        js2-mode
-                        coffee-mode
-                        ruby-mode
-                        inf-ruby
-                        ruby-compilation
-                        rhtml-mode
-                        yaml-mode
-                        ruby-end)
-    "A list of packages to ensure are installed at launch.")
+  ;; my package
+  (setq my-packages
+        (append
+          '(el-get
+            autopair
+            yasnippet auto-complete
+            markdown-mode
+            color-theme color-theme-solarized color-theme-tomorrow powerline
+            slim-mode
+            css-mode sass-mode scss-mode
+            js2-mode jshint-mode coffee-mode
+            ruby-mode inf-ruby ruby-compilation rhtml-mode yaml-mode ruby-end)
 
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p)))
+         (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources))))
 
-  (add-to-list 'load-path "~/.emacs.d/melpa-init-files")
-  (load "init-setup")
+  (el-get 'sync my-packages)
 
-;;; }}}
+;; }}}
